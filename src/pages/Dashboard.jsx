f6c92dae539b13125212240607b11e4013c4af04
bluewhale <paulkingdev7@gmail.com>
2023-10-21 16:00:08 +0300

    taiwlind styling

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,8 +9,8 @@ export default function Dashboard() {
       <div className="flex w-full">
         <Topbar />
         <Sidebar />
-        <div className="w-full">
-          <div className="pt-24 py-4 bg-yellow-50 w-full">
+        <div className="w-full  bg-slate-100 ">
+          <div className="pt-24 py-4 w-full">
             <span className="p-4 text-3xl">Dashboard</span>
           </div>
           <div className="">
